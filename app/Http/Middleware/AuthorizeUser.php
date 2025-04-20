c27e892d47503ab8aa27338dee375a0501a0d615
FlaBBB <f12345ff67@gmail.com>
2025-04-21 06:35:34 +0700

    J7_Prak 3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 class AuthorizeUser
 {
     /**
@@ -12,13 +13,14 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user();
+        $user_role = $request->user()->getRole();
 
-        if($user->hasrole($role)){
+        if (in_array($user_role, $roles)) {
             return $next($request);
         }
+
         abort(403,'Forbidden. Kamu tidak punya akses kehalaman ini');
     }
 }
